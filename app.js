71c9e7b91b43af412dbcc8fddc66ce2b7963818a
Clayton kwok <tommy923xd@hotmail.com>
2021-05-27 17:01:17 +0800

    backup3

--- app.js
+++ app.js
@@ -23,7 +23,10 @@ app.get('/',function (req,res){
 })
 
 app.post('/' ,function (req,res) {
-    res.redirect('/mainpage')
+    if (req.body.username==="todolist@generation.com"&&req.body.password==="28052021"){
+        res.redirect('/mainpage')
+    }
+    
 })
 
 
